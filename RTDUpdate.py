40d5e50100daf9842865f16d74e07bd3788b1313
WhitneyChia <tygyrbalm@hotmail.com>
2018-07-09 00:41:28 -0400

    --fixed emailer

--- RTDUpdate.py
+++ RTDUpdate.py
@@ -16,10 +16,10 @@ def create_dataframe(file_location, file_name, sheet_name):
 def send_email(BCC, subject, body):
     # Paste data into Outlook email and send to distribution list in BCC
     outlook = win32.Dispatch('outlook.application')
-    print(outlook)
+    body = body.to_html()
     mail = outlook.CreateItem(0)
     mail.BCC = BCC # Change to BCC list
-    mail.Subject = market_update_subject
+    mail.Subject = subject
     mail.HtmlBody = body
     mail.send
     
@@ -30,5 +30,6 @@ def market_update_mailer():
     global market_update_BCC
     
     market_update = create_dataframe(market_update_file_location, market_update_file_name, market_update_sheet_name)
-    print(market_update)
-    send_email(market_update_BCC, market_update_subject, market_update_BCC)
+    send_email(market_update_BCC, market_update_subject, market_update)
+
+market_update_mailer()
